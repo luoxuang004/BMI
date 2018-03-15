9cc3c894b86c61a5c253985a93763600f0f4324c
m305-62\w401 <jijoj@ddddd>
2018-03-15 17:55:24 +0800

    first commit

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine(" ╔════════╗ ");
+            Console.WriteLine(" ║                ║ ");
+            Console.WriteLine(" ║     D M A　　  ║ ");
+            Console.WriteLine(" ║                ║ ");
+            Console.WriteLine(" ╚════════╝ ");
+
+            // 宣告匯率變數與數值
+            double rate = 0.02748;
+
+            // 顯示匯率
+            Console.WriteLine("目前韓幣匯率:" + rate);
+
+            // 接收數值
+            Console.Write("請輸入匯率金額:");
+            string input = Console.ReadLine();
+
+            //計算
+            double total = double.Parse(input) * rate;
+            Console.WriteLine(" 換算成台幣是:  " + total );
+            Console.ReadLine();
+        }
+    }
+}
